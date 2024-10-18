1e204e9286d9fada3094f5ecf3e1e9f159dba0ed
Jackson Price <price.jacksonk@gmail.com>
2024-10-17 20:27:12 -0500

    init commit

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let imageSrcs;
+
+    import { LuminousGallery } from 'luminous-lightbox';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        new LuminousGallery(document.querySelectorAll(".gallery"));
+    });
+</script>
+
+<div class="image-grid">
+    {#each imageSrcs as src (src)}
+        <a class="gallery" href={src}>
+            <img src={src} alt="poo{src}" loading="lazy">
+        </a>
+    {/each}
+</div>
+
+<style>
+    .image-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 10px;
+        padding: 1rem 0;
+        grid-template-rows: masonry;
+    }
+
+    .image-grid img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+        border-radius: 8px;
+    }
+</style>
