a0bd6b56bfdfc25b9b977ee81364c2b4475415b7
Jackson Price <price.jacksonk@gmail.com>
2024-10-25 11:46:32 -0500

    added headinfo component

--- public/images/eagle_rock_loop/image_bash.sh
+++ public/images/eagle_rock_loop/image_bash.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Initialize an empty array
 webp_files=()
 
-# Loop through all .webp files in the current directory
 for file in *.webp; do
   # Check if there are any .webp files
   if [[ -f $file ]]; then
-    # Append the formatted string to the array with quotes
     webp_files+=("\"/images/eagle_rock_loop/$file\",")
   fi
 done
 
-# Output the array
 echo "[${webp_files[*]}]"
 
